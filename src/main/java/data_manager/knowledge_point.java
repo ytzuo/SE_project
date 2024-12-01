806fa194e1a4e33d6d2c9c4c3c079e0709a2f8ef
ytz <1472290813@qq.com>
2024-12-01 11:00:21 +0800

    添加了一些说明
    11/27: 设计了一些操作数据的方法
    
    11/28: 新增了连接数据库的connector类
    
    12/1: 完成了question.java中的方法

--- src/main/java/data_manager/knowledge_point.java
+++ src/main/java/data_manager/knowledge_point.java
@@ -1,5 +1,6 @@
 package data_manager;
 
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Transaction;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.List;
 public class knowledge_point {
     public static List<String> get_knowledge(String chapter, Transaction tx){
         //根据传入的章节, 查找其包含的知识点, 组成list返回
+        Label label = Label.label("chapter");
         List<String> knowledge_list = new ArrayList<>();
         //TODO 获取并返回指定章节的知识点
         return knowledge_list;
@@ -21,6 +23,7 @@ public class knowledge_point {
 
     public static List<String> get_knowledge_point(String knowledge_point, Transaction tx){
         //根据传入的知识点, 查找具体的知识点内容, 组成list返回
+        Label label = Label.label("knowledge");
         List<String> knowledge_point_list = new ArrayList<>();
         //TODO 获取并返回指定的知识点的内容
         return knowledge_point_list;
--- src/main/java/data_manager/question.java
+++ src/main/java/data_manager/question.java
@@ -1,38 +1,95 @@
 package data_manager;
 
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++
 //Transaction tx是用来和数据库交互的对象
 //需要在外部由connector返回的graphDb创建
-//++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++
+
+//+++++++++++++++++++++++++++++++++++
+//每个题目的节点的父节点均为chapter节点
+//且每个题目节点都有属性question和answer
+//+++++++++++++++++++++++++++++++++++
 
 public class question {
+    //题目类型的枚举类
+    private enum rel implements RelationshipType
+    {
+        select, blank, summary
+    }
+
     public static List<Integer> get_question_num(String chapter, Transaction tx){
         //可以用来支持随机选题, 由于数据库中存储的题目数可能超过一章后习题的数量
+        Label label = Label.label("chapter");
+        Node parentNode = tx.findNode(label, "chapter_name", chapter); //三个参数为: 节点的标签, 节点的属性名, 节点的属性值
         List<Integer> question_num_list = new ArrayList<>();
-        //TODO 根据章节获取对应各类题目的数量, 依次存入list后返回
+        int select_num = 0;
+        int blank_num = 0;
+        int summary_num = 0;
+        if(parentNode != null){ //计算各个类型题目的数量
+            for(Relationship r : parentNode.getRelationships(rel.select)) select_num++;
+            for(Relationship r : parentNode.getRelationships(rel.blank)) blank_num++;
+            for(Relationship r : parentNode.getRelationships(rel.summary)) summary_num++;
+        }
+        question_num_list.add(select_num);
+        question_num_list.add(blank_num);
+        question_num_list.add(summary_num);
+
         return question_num_list;
     }
 
     public static List<String> get_select(String chapter, Transaction tx){
+        Label label = Label.label("chapter");
+        Node parentNode = tx.findNode(label, "chapter_name", chapter); //参数含义同上
         List<String> select_list = new ArrayList<>();
-        //TODO 根据章节获取对应的选择题, 将题干,选项和答案按顺序存入list并返回
+        if(parentNode != null){
+            for(Relationship r : parentNode.getRelationships(rel.select)) {
+                Node childNode = r.getOtherNode(parentNode);
+                String question = childNode.getProperty("question").toString();
+                String answer = childNode.getProperty("answer").toString();
+                select_list.add(question);
+                select_list.add(answer);
+            }
+        }
+
         return select_list;
     }
 
     public static List<String> get_blank(String chapter, Transaction tx){
+        Label label = Label.label("chapter");
+        Node parentNode = tx.findNode(label, "chapter_name", chapter); //参数含义同上
         List<String> blank_list = new ArrayList<>();
-        //TODO 根据章节获取对应的填空题, 将题干和答案按顺序存入list并返回
+        if(parentNode != null){
+            for(Relationship r : parentNode.getRelationships(rel.blank)) {
+                Node childNode = r.getOtherNode(parentNode);
+                String question = childNode.getProperty("question").toString();
+                String answer = childNode.getProperty("answer").toString();
+                blank_list.add(question);
+                blank_list.add(answer);
+            }
+        }
+
         return blank_list;
     }
 
     public static List<String> get_summary(String chapter, Transaction tx){
+        Label label = Label.label("chapter");
+        Node parentNode = tx.findNode(label, "chapter_name", chapter); //参数含义同上
         List<String> summary_list = new ArrayList<>();
-        //TODO 根据章节获取对应的简述题, 将题干和答案按顺序存入list并返回
+        if(parentNode != null){
+            for(Relationship r : parentNode.getRelationships(rel.blank)) {
+                Node childNode = r.getOtherNode(parentNode);
+                String question = childNode.getProperty("question").toString();
+                String answer = childNode.getProperty("answer").toString();
+                summary_list.add(question);
+                summary_list.add(answer);
+            }
+        }
+
         return summary_list;
     }
 }
